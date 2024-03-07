a92bff3601265d1089a9a974cf637b62eb60b1fd
tequ <git@tequ.dev>
2024-03-08 00:03:52 +0900

    update to use COPY_UINT256

--- contracts/index.c
+++ contracts/index.c
@@ -10,9 +10,18 @@
 #define HASH_DATA_GUARD 64 * PROOF_LENGTH + 2
 #define HASH_OUT_GUARD 32 * PROOF_LENGTH + 2
 
+#define COPY_UINT256(buf_raw, i)                                               \
+  {                                                                            \
+    unsigned char *buf = (unsigned char *)buf_raw;                             \
+    *(uint64_t *)(buf + 0) = *(uint64_t *)(i + 0);                             \
+    *(uint64_t *)(buf + 8) = *(uint64_t *)(i + 8);                             \
+    *(uint64_t *)(buf + 16) = *(uint64_t *)(i + 16);                           \
+    *(uint64_t *)(buf + 24) = *(uint64_t *)(i + 24);                           \
+  }
+
 int64_t hook(uint32_t reserved) {
 
-  REQUIRE(otxn_type == sfBlob, "Invalid transaction type");
+  REQUIRE(otxn_type() == ttINVOKE, "Invalid transaction type");
 
   // buffer = blob_len + sfBlob
   ASSERT(otxn_slot(1) == 1);
@@ -37,47 +46,47 @@ int64_t hook(uint32_t reserved) {
   // leaf index
   uint8_t index = ptr[32 * PROOF_LENGTH + LEAF_LENGTH]; // last byte
 
-  // hash = leaf
+  // leaf
   uint8_t leaf[LEAF_LENGTH];
   for (int i = 0; GUARD(LEAF_LENGTH), i < LEAF_LENGTH; i++) {
     leaf[i] = ptr[32 * PROOF_LENGTH + i]; // leaf
   }
 
+  // hash = hash(leaf)
   uint8_t hash[32];
   util_sha512h(SBUF(hash), SBUF(leaf));
 
+  // loop for proofs
   for (int i = 0; GUARD(PROOF_LENGTH), i < PROOF_LENGTH; i++) {
     uint8_t currentElement[32];
-    for (int j = 0; GUARD(CURRENT_ELEMENT_GUARD), j < 32; j++) {
-      currentElement[j] = ptr[i * 32 + j];
-    }
+    COPY_UINT256(currentElement, ptr + i * 32);
 
-    // data = hash + currentElement (if left leaf)
-    // data = currentElement + hash (if right leaf)
     uint8_t data[64];
-    for (int j = 0; GUARD(HASH_DATA_GUARD), j < 64; j++) {
-      if (index % 2 == 0)
-        data[j] = j < 32 ? hash[j] : currentElement[j - 32];
-      else
-        data[j] = j < 32 ? currentElement[j] : hash[j - 32];
+
+    if (index % 2 == 0) {
+      // data = hash + currentElement (if left leaf)
+      COPY_UINT256(data, hash);
+      COPY_UINT256(data + 32, currentElement);
+    } else {
+      // data = currentElement + hash (if right leaf)
+      COPY_UINT256(data, currentElement);
+      COPY_UINT256(data + 32, hash);
     }
 
     uint8_t hash_out[32];
     ASSERT(util_sha512h(SBUF(hash_out), SBUF(data)) > 0);
 
     // hash = hash_out
-    for (int j = 0; GUARD(HASH_OUT_GUARD), j < 32; j++) {
-      hash[j] = hash_out[j];
-    }
+    COPY_UINT256(hash, hash_out);
     index = index / 2;
   }
 
-  // if (hash != PROOF_ROOT) rollback(SBUF("Invalid proof"), __LINE__);
+  // if (hash != proof_root) rollback(SBUF("Invalid proof"), __LINE__);
   for (int i = 0; GUARD(32), i < 32; i++) {
     if (hash[i] != proof_root[i])
       rollback(SBUF("Invalid proof"), __LINE__);
   }
-  // REQUIRE(BUFFER_EQUAL_32(hash, PROOF_ROOT), "Invalid proof");
+  // REQUIRE(BUFFER_EQUAL_32(hash, proof_root), "Invalid proof");
 
   accept(SBUF("Valid proof"), __LINE__);
 
